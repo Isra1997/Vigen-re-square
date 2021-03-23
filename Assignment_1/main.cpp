acb85d0af5a00b9907d5940cce9577307b6d3601
Isra1997 <israragheb@gmail.com>
2021-03-23 08:04:48 +0200

    Initial Commit

---
+++ Assignment_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment_1
+//
+//  Created by Isra Ragheb on 3/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
